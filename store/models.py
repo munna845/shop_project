e7767fe9057f7ac7f48f02001766f9b841dce8b6
munna845 <munnaahsan845@gmail.com>
2025-11-05 21:56:36 +0600

    queryset optimized

--- store/models.py
+++ store/models.py
@@ -1,28 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #category model
 class category(models.Model):
  name=models.CharField(max_length=100)
- slug=models.SlugField(unique=True)
+ slug=models.SlugField(unique=True,blank=True)
+ 
+ def __str__(self):
+     return self.name
+
 
 #product model
 class product(models.Model):
-   category =models.CharField(max_length=50)
+   category =models.ForeignKey(category, on_delete=models.CASCADE, related_name="product")
    name = models.CharField(max_length=50)
    slug = models.SlugField(unique=True)
-   description = models.TextField(max_length=50)
+   description = models.TextField(max_length=500)
    price = models.DecimalField(max_digits=10, decimal_places=2)
    stock = models.PositiveIntegerField()
    created_at= models.DateTimeField(auto_now_add=True)
+   
+   def __str__(self):
+        return self.name
   
 #order model
 class order(models.Model):
  
-     user = models.CharField(max_length=50)
+     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name="orders")
      created_at = models.DateTimeField(auto_now_add=True)
      paid = models.BooleanField(default=False)
      total = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+     
+     def __str__(self):
+        return f"Order #{self.id} by {self.user.username}"
   
 
 # OrderItem model  
@@ -31,12 +42,18 @@ class OrderItem(models.Model):
     product = models.ForeignKey(product, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.product.name} (x{self.quantity})"
 
 
 #Cart model
 class Cart(models.Model):
-    session_key = models.CharField(max_length=40, db_index=True)  # session-based cart
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Cart of {self.user.username}"
 
 
 #CartItem
@@ -44,6 +61,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)  
+    
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
 
 
  
--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from . models import category,product,order,OrderItem,Cart,CartItem
 from . serializers import categoryserializer,productserializer,orderserializer,OrderItemtserializer,Cartserializer,CartItemserializer
 from rest_framework import viewsets
 from .permissions import IsOwnerOrAdmin
+from django.contrib.auth.models import User
 
 # Create your views here.
 #category viewsets
@@ -12,27 +13,27 @@ class category_viewsets(viewsets.ModelViewSet):
 
 # product viewsets
 class product_viewsets(viewsets.ModelViewSet):
-  queryset= product.objects.all()
+  queryset= product.objects.select_related(category).all()
   serializer_class= productserializer
   permission_classes=  [IsOwnerOrAdmin]
 
 # order viewsets
 class order_viewsets(viewsets.ModelViewSet):
-  queryset= order.objects.all()
+  queryset= order.objects.select_related(User).prefetch_related('order_items__product').all()
   serializer_class= orderserializer
   permission_classes=  [IsOwnerOrAdmin]
 
 
 # OrderItem viewsets
 class product_viewsets(viewsets.ModelViewSet):
-  queryset= OrderItem.objects.all()
+  queryset= OrderItem.objects.select_related('product','order').all()
   serializer_class= orderserializer
   permission_classes=  [IsOwnerOrAdmin]
 
 
 # Cart viewsets
 class Cart_viewsets(viewsets.ModelViewSet):
-  queryset= Cart.objects.all()
+  queryset= Cart.objects.select_related(User).prefetch_related('cart_items__product').all()
   serializer_class= Cartserializer
   permission_classes=  [IsOwnerOrAdmin]
 
@@ -40,6 +41,6 @@ class Cart_viewsets(viewsets.ModelViewSet):
 
 # CartItem viewsets
 class CartItem_viewsets(viewsets.ModelViewSet):
-  queryset= CartItem.objects.all()
+  queryset= CartItem.objects.select_related('cart','product').all()
   serializer_class= CartItemserializer
   permission_classes=  [IsOwnerOrAdmin]
