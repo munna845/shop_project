23f9cb63dd4fbd54ab14fe9503ea69c2c95b1a71
munna845 <munnaahsan845@gmail.com>
2025-11-05 23:52:59 +0600

    edited permission

--- store/permissions.py
+++ store/permissions.py
@@ -1,12 +1,17 @@
 from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
-    
+    owner_allowed_actions = ['retrieve', 'update', 'partial_update', 'destroy']
 
+    
     def has_object_permission(self, request, view, obj):
-       
+        #admins alwys allowed
         if request.user.is_staff:
             return True
 
-        
-        return obj.user == request.user
+        # Owners allowed certain actions only
+        if obj.user == request.user:
+            if view.action in self.owner_allowed_actions:
+                return True
+
+        return False
